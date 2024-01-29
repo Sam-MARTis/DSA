031218ba26283f424a942e304c50b5b8d36109a8
Sam-MARTis <samanthmartis@gmail.com>
2024-01-29 11:48:58 +0530

    Added documentation

--- 01-Python_DSA/sorting_algorithms/reverSort.py
+++ 01-Python_DSA/sorting_algorithms/reverSort.py
@@ -1,44 +1,52 @@
 from numpy import argmin
 from ArrayBaseModel import ArrayModel
-# import logging
 
 
 class Array(ArrayModel):
-    def __init__(self, a: list = []) -> None:
-        super().__init__(a)
-     
-    # @staticmethod
-    # def reverse(a, index_low:int, index_high:int) -> list:
-    #     if index_high >= len(a)  or  index_low <0: 
-    #         logging.warning(IndexError(" Index out of bounds. Careful..."))
-    #     print(f"a[:index_low]: {a[:index_low+1]}")
-    #     print(f"a[index_high-1 : index_low: -1]: {a[index_high-1 : index_low: -1]}")
-    #     print(f" a[index_high:]: { a[index_high:]}")
-    #     return [a[0]*(index_low==0)] + a[:index_low+1] + a[index_high-1 : index_low: -1] + a[index_high:]
+    def __init__(self, valsToStore: list = []) -> None:
+        super().__init__(valsToStore)
+        
+        
+        
     @staticmethod
-    def reverse(a:list, index_low:int, index_high:int) -> list:
-        arr = []
+    def reverse(array_to_reverse:list, index_low:int, index_high:int) -> list:
+        '''
+        Returns a array that has terms of the input array between index_low to index_high (inclusive) reversed.
+        
+        :param array_to_reverse: list to reverse
+        :param index_low: which element to reverse from (inclusive)
+        :param index_high: till element to reverse to (inclusive)
+        
+        Note: doesn't mutate the input array.
+        
+        '''
+        
+        changed_array = []
         for i in range(index_low):
-            arr.append(a[i])
-        for i in range( 1+ index_high-index_low):
-            arr.append(a[index_high-i])
-        for i in range((len(a)-1)-index_high):
-            arr.append(a[1+index_high +i])
-        return arr
+            changed_array.append(array_to_reverse[i])
+            
+        for i in range( 1+ (index_high - index_low) ):
+            changed_array.append(array_to_reverse[index_high - i])
+            
+        for i in range((len(array_to_reverse) - 1) - index_high):
+            changed_array.append(array_to_reverse[ 1 + index_high + i])
+            
+        return changed_array
     
             
     
     def reverSort(self):
-        # pass
+        '''
+        Revere Sort is an time O(n^2) and (in my case) a space O(n^2) algorithm to sort an array
         
+        It works by iterating i over elements from START to END.
+        For every i, it checks if what the smallest element is from i to END.
+        Next, it calls the reverse function of that subarray from i to minValue_i. ie, it flips the terms from i to minValue_i.
+        This way minValue_i is now in its correct spot. 
+        Doing this for all elements, the array is ssorted, with each iteration sorting, at the very least, one element to its correct spot.
+        '''
         for i in range(round(len(self.vals)-1)):
-            self.setValues(self.reverse(self.vals, i, i+argmin(self.vals[i:])))
-            
-            # print(f"self.vals: {self.vals} \ni = {i}\nArr[i:]: {self.vals[i :]}\ni+argmin(Arr[i:]): {i+argmin(self.vals[i :])}\nreversed values: {self.}\n\n\n")
-    
-            # self.setValues(k)
-            # Arr = k.copy()
-            
+            self.setValues(self.reverse(self.vals, i, i+argmin(self.vals[i:])))            
         return self.getValues()
             
 
@@ -46,13 +54,10 @@ class Array(ArrayModel):
 
 
 
+#Testing. Uncomment/comment to see/hide examples
+arr = Array([0,0,23,2,3,96,95,95,93,92,4,5,6,7,44,20,10])
 
-arr = Array([0,23,2,3,96,95, 95,93,92,4,5,6,7,44,20,10])
-# """Testing
-# arrmine = [0, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f'Unsorted values: {arr.getValues()}\n')
+arr.reverSort()
+print(f"Sorted values: {arr.getValues()}")
 
-# print(arr.reverse(arrmine, index_low=1, index_high=10))
-# should give [0,11,2,3,7,6,5,4,8,9,10] for 4, 7
-print(arr.reverSort())
-
-# """
