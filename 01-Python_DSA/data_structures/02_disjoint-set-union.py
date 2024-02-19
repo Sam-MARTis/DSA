1ca8ccfab2958ba0d0825ed2f9b3f3a706d268bf
Sam-MARTis <samanthmartis@gmail.com>
2024-02-19 11:44:12 +0530

    added merging of sets

--- 01-Python_DSA/data_structures/02_disjoint-set-union.py
+++ 01-Python_DSA/data_structures/02_disjoint-set-union.py
@@ -2,20 +2,34 @@ class DSU:
     def __init__(self) -> None:
         self.sets = []
     
-    def make_set(self, x):
+    def make_set(self, *x):
         setThis = set()
-        setThis.add(x)
+        for i in x:
+            setThis.add(i)
         self.sets.append(setThis)
-    def combine_set(self, x, y):
-        if (x not in self.sets) or (y not in self.sets):
-            return ValueError("Elements need to be in the set first")
     def get_sets(self):
         return self.sets
+    def mergse_sets(self, x, y):
+        set1 = set()
+        set2 = set()
+        for i in self.sets:
+            if x in i:
+                set1 = i
+            if y in i:
+                set2 = i
+        setCombined = set1.union(set2)
+        self.sets.remove(set1)
+        self.sets.remove(set2)
+        self.sets.append(setCombined)
+            
     
 
 myDSU = DSU()
 print(myDSU.get_sets())
 myDSU.make_set(2)
 print(myDSU.get_sets())
-myDSU.make_set(3)
+myDSU.make_set(3, 4, 3, 5)
+myDSU.mergse_sets(2, 3)
+myDSU.make_set(3, 4, 9)
+myDSU.mergse_sets(2, 3)
 print(myDSU.get_sets())
