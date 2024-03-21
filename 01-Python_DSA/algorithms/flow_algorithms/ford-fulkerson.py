43ae8ed9f45fc5cba7a965ad95845070790aa623
Sam-MARTis <samanthmartis@gmail.com>
2024-03-21 21:34:09 +0530

    removed unecessary commented part

--- 01-Python_DSA/algorithms/flow_algorithms/ford-fulkerson.py
+++ 01-Python_DSA/algorithms/flow_algorithms/ford-fulkerson.py
@@ -31,14 +31,11 @@ node2Edge1 = node2.edges[0]
 node3 = node2.traverse(node2Edge1)[0]
 node3Edge1 = node3.edges[0]
 node4 = node3.traverse(node3Edge1)[0]
-print(node2.id)
-print(node3.id)
-print(node4.id)
+# print(node2.id)
+# print(node3.id)
+# print(node4.id)
 
-# print(GraphNodes[1].traverse(GraphNodes[1].edges[0]))
 
-# def Floyd_Fulkerson(Graph):
-#     source = Graph.source
 
 def DFS_connected_check(graph, startID, endID) -> bool:
     Nodes: List[Node] = graph.getNodes()
@@ -47,28 +44,22 @@ def DFS_connected_check(graph, startID, endID) -> bool:
     queueToExplore = [[startID, StartNode.getEdges() ]] #Node ID, unexplored edges
     while len(queueToExplore)>0:
         currentItem = queueToExplore[-1]
-        # print(currentItem)
         currentNode: Node = Nodes[currentItem[0]]
         currentNodeEdges = currentItem[1]
 
         if currentNode.id == endID:
-            # print("Found ittt")
+            print("Found ittt")
             return True
         
         if currentNodeEdges==[] or currentNodeEdges == [[]]:
-            # print(queueToExplore)
             queueToExplore.pop()
             continue
         for edge in currentNodeEdges:
-            # print(f"CurrentNodeID = {currentNode.id}")
             newNode: Node = currentNode.traverse(edge)[0]
-            # print(f"NewNodeID = {newNode.id}")
-            # print(f"{newNode.id}.state['visited'] = {newNode.state['visited']}")
             if newNode.state['visited']==False:
-                # print(f"Setting {newNode.id}.state['visited'] = True")
+
                 newNode.state['visited'] = True
                 queueToExplore.append([newNode.id, newNode.getEdges()])
-                print(queueToExplore)
                 break
             else:
                 currentNodeEdges.remove(edge)
